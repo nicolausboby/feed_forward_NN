0b44ff500010a78f697f9e9ef6f4b619b4434c49
Nicolaus Boby <nicolausboby@gmail.com>
2019-09-28 00:30:27 +0700

    debug

--- example.py
+++ example.py
@@ -1,5 +1,25 @@
 import neural_network
+import pandas as pd
+import numpy as np
 
-model = neural_network.FeedForwardNeuralNetwork(2, [2,2])
+model = neural_network.FeedForwardNeuralNetwork(hidden_layers=2, nb_nodes=[1,1])
 
 
+# data = pd.read_csv("weather.csv")
+# print(data)
+
+X = [[1, 80, 85.1, 1], [2, 83, 86, 2]]
+y = [0, 1]
+print(model)
+for layer in model.layers:
+    print(layer)
+    print(len(layer.nodes))
+    # for node in layer.nodes:
+    #     print(node)
+model.fit(X, y)
+print("\n\nSESUDAH")
+for layer in model.layers:
+    print(layer)
+    print(len(layer.nodes))
+    # for node in layer.nodes:
+    #     print(node)
