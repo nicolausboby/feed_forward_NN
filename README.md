cc26dc8f5a8c50d2bea831dbaea101f62b109996
Kevin Basuki <kevinb9g.17@gmail.com>
2019-09-30 21:08:33 +0700

    Add readme & clean example.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-vanilla, naive, and simple implementation of Feed Forward Neural Network
+# Simple implementation of Feed Forward Neural Network using mini-batch gradient descent
+Run example.py to test the code using weather.csv
+
+### Anggota Kelompok
+1. Kevin Basuki - 13516071
+2. Nicolaus Boby - 13516077
+3. Shandy - 13516097
--- example.py
+++ example.py
@@ -3,7 +3,6 @@ from neural_network.helper import read_csv, label_encoding, one_hot_encoding, ac
 
 # Data Reading
 data = read_csv("weather.csv")
-# print(data)
 
 X = []
 for item in data:
@@ -14,42 +13,15 @@ for item in data:
     y.append(item[-1])
 
 # Data Preprocessing
-# print(X)
-# print(y)
-# print("Label Encoding")
-# print("encode X")
 one_hot_encoding(X)
-# print("encode Y")
 label_encoding(y)
-# print(X)
-# print(y)
 
 model = neural_network.FeedForwardNeuralNetwork(hidden_layers=2, nb_nodes=[2, 2])
-
-# print("\nBEFORE FITTING")
-# print(model)
-# for layer in model.layers:
-#     print(layer)
-#     print(len(layer.nodes))
-#     for node in layer.nodes:
-#         print(node)
-# print("output :", model._output_layer.nodes[0].output)
-
-# model.fit(X, y)
 model.mb_gradient_descent(inputs=X, targets=y, batch_size=2, epoch=5)
-
-print("\n\nAFTER FITTING")
-for layer in model.layers:
-    print(layer)
-    print(len(layer.nodes))
-    for node in layer.nodes:
-        print(node)
-print("output :", model._output_layer.nodes[0].output)
 
 print("\n\nPREDICTION (Using Weather Dataset)\n")
 outputs = []
 for i in range(len(X)):
-    # print(X[i])
     outputs.append(round(model.predict(X[i])))
 print(outputs)
 print(y)
